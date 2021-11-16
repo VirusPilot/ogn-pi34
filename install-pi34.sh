229c4238665c56d16162db9a858e4451c80b8871
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2021-11-16 09:07:34 +0100

    Update install-pi34.sh

--- install-pi34.sh
+++ install-pi34.sh
@@ -34,9 +34,9 @@ if [ $ARCH == aarch64 ]; then # arm64
   tar xvf ogn-frb-search/rtlsdr-ogn/rtlsdr-ogn-bin-arm64-0.2.9_Buster.tgz
 else # armhf
   if grep -q "Pi 4" /proc/device-tree/model; then
-    tar xvf ogn-frb-search/rtlsdr-ogn/rtlsdr-ogn-bin-RPI-GPU-0.2.9_Jessie.tgz
-  else # "Pi 3"
     tar xvf ogn-frb-search/rtlsdr-ogn/rtlsdr-ogn-bin-ARM-0.2.9_Jessie.tgz
+  else # "Pi 3"
+    tar xvf ogn-frb-search/rtlsdr-ogn/rtlsdr-ogn-bin-RPI-GPU-0.2.9_Jessie.tgz
   fi
 fi
 
