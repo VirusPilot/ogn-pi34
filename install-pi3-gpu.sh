e0446a5ef25ffe1cefe423097b19efd94cc8eddc
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2024-03-12 17:49:10 +0100

    some cosmetics

--- install-pi3-gpu.sh
+++ install-pi3-gpu.sh
@@ -26,11 +26,6 @@ echo blacklist dvb_usb_rtl28xxu | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.
 echo blacklist dvb_usb_v2 | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
 echo blacklist rtl8xxxu | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
 
-# Bookworm: Debian 12 (32bit and 64bit)
-# Bullseye: Debian 11 (32bit and 64bit)
-# Buster:   Debian 10 (32bit)
-# Stretch:  Debian 9  (32bit)
-
 # prepare rtlsdr-ogn
 if [ "$ARCH" -eq 32 ] && [ "$DIST" -lt 11 ]; then
   wget http://download.glidernet.org/rpi-gpu/rtlsdr-ogn-bin-RPI-GPU-0.3.0.tgz
@@ -41,7 +36,6 @@ else
   echo
   exit
 fi
-
 cd rtlsdr-ogn
 sudo chown root gsm_scan ogn-rf rtlsdr-ogn
 sudo chmod a+s gsm_scan ogn-rf rtlsdr-ogn
@@ -54,7 +48,7 @@ echo
 read -p "Press any key to continue"
 sudo nano Template.conf
 
-sudo wget http://download.glidernet.org/common/WW15MGH.DAC
+wget http://download.glidernet.org/common/WW15MGH.DAC
 
 # install rtlsdr-ogn service
 sudo cp -v rtlsdr-ogn /etc/init.d/rtlsdr-ogn
