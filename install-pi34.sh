f690f96d6acff731d55e8ce365d77b7aa560cafe
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2024-12-10 19:37:11 +0100

    consolidate scripts

--- install-pi34.sh
+++ install-pi34.sh
@@ -8,42 +8,16 @@ sudo apt install debhelper -y
 ARCH=$(getconf LONG_BIT)
 DIST=$(lsb_release -r -s)
 
-# install librtlsdr from http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr
-# if [ $ARCH -eq 64 ] && [ "$DIST" -eq 12 ]; then
-#     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_2.0.2-2+b1_arm64.deb
-#     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr-dev_2.0.2-2+b1_arm64.deb
-#     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/rtl-sdr_2.0.2-2+b1_arm64.deb
-# fi
-# if [ $ARCH -eq 32 ] && [ "$DIST" -eq 12 ]; then # no RTL-SDR Blog V4 dongle support
-#     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_0.6.0-4_armhf.deb
-#     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr-dev_0.6.0-4_armhf.deb
-#     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/rtl-sdr_0.6.0-4_armhf.deb
-# fi 
-# if [ $ARCH -eq 64 ] && [ "$DIST" -eq 11 ]; then # no RTL-SDR Blog V4 dongle support
-#     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_0.6.0-4_arm64.deb
-#     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr-dev_0.6.0-4_arm64.deb
-#     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/rtl-sdr_0.6.0-4_arm64.deb
-# fi
-# if [ $ARCH -eq 32 ] && [ "$DIST" -eq 11 ]; then # no RTL-SDR Blog V4 dongle support
-#     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_0.6.0-4_armhf.deb
-#     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr-dev_0.6.0-4_armhf.deb
-#     wget http://ftp.de.debian.org/debian/pool/main/r/rtl-sdr/rtl-sdr_0.6.0-4_armhf.deb
-# fi
-# sudo dpkg -i *.deb
-# rm -f *.deb
-# sudo ldconfig
-
 # compile and install librtlsdr from https://github.com/osmocom/rtl-sdr
 cd
 git clone https://github.com/osmocom/rtl-sdr
 cd rtl-sdr
-mkdir build
-cd build
-cmake ../ -DDETACH_KERNEL_DRIVER=ON -DINSTALL_UDEV_RULES=ON
-make
-sudo make install
-sudo ldconfig
+sudo dpkg-buildpackage -b --no-sign
 cd
+sudo dpkg -i *.deb
+rm -f *.deb
+rm -f *.buildinfo
+rm -f *.changes
 
 echo blacklist rtl2832 | sudo tee /etc/modprobe.d/rtl-sdr-blacklist.conf
 echo blacklist r820t | sudo tee -a /etc/modprobe.d/rtl-sdr-blacklist.conf
