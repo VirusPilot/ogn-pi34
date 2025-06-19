f19afb9422243c963bb6fe931aa221cf915814a6
VirusPilot <43483458+VirusPilot@users.noreply.github.com>
2025-06-19 21:11:21 +0200

    Update install-pi34-ogn2dump1090.sh

--- install-pi34-ogn2dump1090.sh
+++ install-pi34-ogn2dump1090.sh
@@ -59,7 +59,7 @@ echo "and don't forget to add --net-sbs-in-port 30008 to NET_OPTIONS"
 echo
 echo "RECEIVER_OPTIONS="--device 1090 --device-type rtlsdr --gain auto-verbose --ppm 0""
 echo "DECODER_OPTIONS="--lat 48.0 --lon 10.0 --max-range 450 --write-json-every 1""
-echo "NET_OPTIONS=--net --net-sbs-in-port 30008"
+echo NET_OPTIONS="--net --net-ri-port 30001 --net-ro-port 30002 --net-sbs-port 30003 --net-bi-port 30004,30104 --net-bo-port 30005 --net-sbs-jaero-in-port 30008 --jaero-timeout 1"
 echo
 read -p "Press any key to continue"
 sudo nano /etc/default/readsb
